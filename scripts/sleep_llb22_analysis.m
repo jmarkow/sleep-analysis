ce31d25330b7a16b00cfa322898c0acb5c7ccb04
Jeffrey Markowitz <jmarkow@cns.bu.edu>
2016-06-17 19:35:48 -0400

    Added plotting

--- scripts/sleep_llb22_analysis.m
+++ scripts/sleep_llb22_analysis.m
@@ -3,7 +3,8 @@
 % bin rate
 
 bin_fs=500;
-npeaks=50;
+npeaks=100;
+padding=.2;
 
 % we should probably smooth...
 
@@ -13,13 +14,14 @@ smooth_kernel=smooth_kernel./sum(smooth_kernel);
 
 % average, no more no less
 
+pad_smps=round(spikes_fs*padding);
 nsamples=spikes_day{1}.parameters.raw_data_sz(1);
 ntrials=spikes_day{1}.parameters.raw_data_sz(2);
 spikes_fs=spikes_day{1}.parameters.fs;
 
 % put spikes in your standard issue binary vector
 
-bin_vec=[0:1/bin_fs*spikes_fs:nsamples];
+bin_vec=[pad_smps:1/bin_fs*spikes_fs:nsamples-pad_smps];
 spikes_template=zeros(length(bin_vec),ntrials);
 
 for i=1:ntrials
@@ -32,7 +34,7 @@ end
 
 nsamples=spikes_night{1}.parameters.raw_data_sz(1);
 ntrials=spikes_night{1}.parameters.raw_data_sz(2);
-bin_vec=[0:1/bin_fs*spikes_fs:nsamples]
+bin_vec=[pad_smps:1/bin_fs*spikes_fs:nsamples-pad_smps];
 
 spikes_target=zeros(length(bin_vec),ntrials);
 
@@ -41,7 +43,7 @@ for i=1:ntrials
   spikes_target(:,i)=conv(spikes_target(:,i),smooth_kernel,'same');
 end
 
-% string 'em together, like beads on a string...a correlation string 
+% string 'em together, like beads on a string...a correlation string
 
 target_vec=spikes_target(:);
 target_ids=[];
@@ -49,28 +51,19 @@ for i=1:ntrials
      target_ids=[target_ids;repmat(i,[length(bin_vec) 1])];
 end
 
+[r_rev]=xcorr(zscore(mean(spikes_template(end:-1:1,:),2)),zscore(target_vec));
+[r,lags]=xcorr(zscore(mean(spikes_template(1:end,:),2)),zscore(target_vec));
 
-[r,lags]=xcorr(zscore(mean(spikes_template,2)),zscore(target_vec));
 [pks,locs]=findpeaks(r);
 [~,idx]=sort(pks,'descend');
 locs=abs(lags(locs(idx)));
 % take top N peaks
 
-
 win_size=size(spikes_template,1);
 extractions=zeros(win_size,npeaks);
 
-
 for i=1:npeaks
-    
     tmp=target_vec(locs(i)+1:locs(i)+win_size);
     tmp2=target_ids(locs(i)+1:locs(i)+win_size);
     extractions(:,i)=tmp;
-    
 end
-
-
-
-
-
-
